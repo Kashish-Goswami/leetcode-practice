4c501bddce181e1c8418a172cc0851d0f6d57d90
Kashish Goswami <kashishgoswami155@gmail.com>
2026-01-20 19:38:21 +0530

    Day 04: Added Remove Duplicates from Sorted Array
    Implement solution for removing duplicates from a sorted array using a two-pointer approach.

---
+++ Day-04/Remove_Duplicates_From_Sorted_Array.java
@@ -0,0 +1,22 @@
+// Day 04 - Remove Duplicates from Sorted Array
+// LeetCode #26
+// Difficulty: Easy
+// Approach: Two Pointers
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int index = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
