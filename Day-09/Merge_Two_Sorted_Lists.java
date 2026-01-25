5fe9122a6711ed9af67eb5f646324d3d25d4ace7
Kashish Goswami <kashishgoswami155@gmail.com>
2026-01-25 22:05:46 +0530

    Day 09: Added Merge Two Sorted Lists

---
+++ Day-09/Merge_Two_Sorted_Lists.java
@@ -0,0 +1,27 @@
+// Day 09 - Merge Two Sorted Lists
+// LeetCode #21
+// Difficulty: Easy
+// Approach: Iterative Merge
+// Time Complexity: O(n + m)
+// Space Complexity: O(1)
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        curr.next = (list1 != null) ? list1 : list2;
+        return dummy.next;
+    }
+}
