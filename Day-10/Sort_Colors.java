23571f406e7a15dbb3df82b349b66c9790e84da4
Kashish Goswami <kashishgoswami155@gmail.com>
2026-01-26 22:38:21 +0530

    Day 10: Added Sort Colors
    Implement Dutch National Flag algorithm to sort colors.

---
+++ Day-10/Sort_Colors.java
@@ -0,0 +1,26 @@
+// Day 10 - Sort Colors
+// LeetCode #75
+// Difficulty: Medium
+// Approach: Dutch National Flag
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low++] = nums[mid];
+                nums[mid++] = temp;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high--] = temp;
+            }
+        }
+    }
+}
