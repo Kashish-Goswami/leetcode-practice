b727b8346f45d42f341a0d72d09bce54dd087084
Kashish Goswami <kashishgoswami155@gmail.com>
2026-01-30 22:38:27 +0530

    Day 14: Added Climbing Stairs

---
+++ Day-14/Climbing_Stairs.java
@@ -0,0 +1,21 @@
+// Day 14 - Climbing Stairs
+// LeetCode #70
+// Difficulty: Easy
+// Approach: DP
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+
+        int prev1 = 1, prev2 = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return prev2;
+    }
+}
