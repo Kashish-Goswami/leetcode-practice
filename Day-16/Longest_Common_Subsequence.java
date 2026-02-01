e5095ea52c62c435bcdac7afd0206ec0dfb8b979
Kashish Goswami <kashishgoswami155@gmail.com>
2026-02-01 21:42:15 +0530

    Day 16: Added Longest Common Subsequence

---
+++ Day-16/Longest_Common_Subsequence.java
@@ -0,0 +1,24 @@
+// Day 16 - Longest Common Subsequence
+// LeetCode #1143
+// Difficulty: Medium
+// Approach: 2D DP (String DP)
+// Time Complexity: O(m * n)
+// Space Complexity: O(m * n)
+
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
