190370821ae89c8b12ab1838f34969c82d10ca4f
Kashish Goswami <kashishgoswami155@gmail.com>
2026-02-04 22:47:11 +0530

    Day 19: Added Counting Bits

---
+++ Day-19/Counting_Bits.java
@@ -0,0 +1,16 @@
+// Day 19 - Counting Bits
+// LeetCode #338
+// Difficulty: Medium
+// Concept: DP + Bit Manipulation
+
+class Solution {
+    public int[] countBits(int n) {
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i >> 1] + (i & 1);
+        }
+
+        return dp;
+    }
+}
