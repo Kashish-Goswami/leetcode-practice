dabcc8705659ba1ec81312adb08d4348de7cf89f
Kashish Goswami <kashishgoswami155@gmail.com>
2026-02-05 22:21:58 +0530

    Day 20: Added Jump Game

---
+++ Day-20/Jump_Game.java
@@ -0,0 +1,22 @@
+// Day 20 - Jump Game
+// LeetCode #55
+// Difficulty: Medium
+// Concept: Greedy Reach
+
+class Solution {
+
+    public boolean canJump(int[] nums) {
+
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (i > maxReach)
+                return false;
+
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+
+        return true;
+    }
+}
