17c5ac8e1748ad3207f0d5a75e9fb1f51b8589b7
Kashish Goswami <kashishgoswami155@gmail.com>
2026-02-06 21:50:18 +0530

    Day 21: Added Product of Array Except Self

---
+++ Day-21/Product_of_Array_Except_Self.java
@@ -0,0 +1,28 @@
+// Day 21 - Product of Array Except Self
+// LeetCode #238
+// Difficulty: Medium
+// Concept: Prefix + Suffix
+
+class Solution {
+
+    public int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+        int[] result = new int[n];
+
+        result[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        int suffix = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return result;
+    }
+}
