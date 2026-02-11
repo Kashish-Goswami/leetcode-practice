d8eaf74938b8a851c168125244c2f67e5fab5436
Kashish Goswami <kashishgoswami155@gmail.com>
2026-02-11 22:23:47 +0530

    Day 22: Added Find the Duplicate Number

---
+++ Day-22/Find_the_Duplicate_Number.java
@@ -0,0 +1,28 @@
+// Day 22 - Find the Duplicate Number
+// LeetCode #287
+// Difficulty: Medium
+// Concept: Floyd Cycle Detection
+
+class Solution {
+
+    public int findDuplicate(int[] nums) {
+
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        while (slow != fast);
+
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
